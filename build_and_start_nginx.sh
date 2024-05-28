418ef7730d298857e51c4b2d609be7e150db1925
Pelle <52049523+farapholch@users.noreply.github.com>
2024-05-28 20:30:48 +0200

    Update build_and_start_nginx.sh

--- build_and_start_nginx.sh
+++ build_and_start_nginx.sh
@@ -3,16 +3,21 @@ set -e
 
 cd /src/build-your-own-radar
 
-#echo "Starting webpack build..."
-#npm run build:prod
+echo "Starting webpack build..."
+npm run build:prod
 
-#echo "Copying built files to nginx directories..."
-#mkdir -p /opt/build-your-own-radar
-#cd /opt/build-your-own-radar
-#cp -r /src/build-your-own-radar/dist/* ./
-#mkdir -p files
-#cp /src/build-your-own-radar/spec/end_to_end_tests/resources/localfiles/* ./files/
-#cp /src/build-your-own-radar/default.template /etc/nginx/conf.d/default.conf
+echo "Copying built files to nginx directories..."
+echo "Creating build-your-own-radar directory..."
+mkdir -p /opt/build-your-own-radar
+
+echo "Creating dist directory..."
+mkdir -p /opt/build-your-own-radar/dist
+
+cd /opt/build-your-own-radar
+cp -r /src/build-your-own-radar/dist/* ./
+mkdir -p files
+cp /src/build-your-own-radar/spec/end_to_end_tests/resources/localfiles/* ./files/
+cp /src/build-your-own-radar/default.template /etc/nginx/conf.d/default.conf
 
 echo "Starting nginx server..."
 exec nginx -g 'daemon off;'
